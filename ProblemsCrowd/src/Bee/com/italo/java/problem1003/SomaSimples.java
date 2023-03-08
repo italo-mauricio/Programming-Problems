b6e33eecf13050d05cc2e335f2caeadf1ef35f1a
italo <italogithub@gmail.com>
2023-03-08 09:04:46 -0300

    add problem 1003 - soma simples

---
+++ ProblemsCrowd/src/Bee/com/italo/java/problem1003/SomaSimples.java
@@ -0,0 +1,47 @@
+package Bee.com.italo.java.problem1003;
+
+
+import java.util.Scanner;
+
+/***
+ * @author italo-mauricio
+ *
+ *
+ *                      Soma Simples
+ *
+ *
+ * Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável
+ * SOMA. A seguir escrever o valor desta variável.
+ *
+ *
+ * ENTRADA:
+ *      - O arquivo de entrada contém 2 valores inteiros.
+ *
+ *
+ * SAIDA:
+ *      - Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido
+ *      pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado,
+ *      caso contrário, receberá "Presentations Error".
+ *
+ *
+ */
+
+
+public class SomaSimples {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+    }
+}
+
+/*
+    Resultado:
+        Linguagem: Java 14 (Open JDK 1.14.0)[+2s]
+        Tempo de execução: 0.201s
+        Tamanho: 374 Bytes
+ */
