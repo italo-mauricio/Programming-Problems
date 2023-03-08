74549e41010380386404f14b3c08c5865907da28
italo <italogithub@gmail.com>
2023-03-08 12:56:34 -0300

    add problem 1008 - salario funcionarios

---
+++ ProblemsCrowd/src/Bee/com/italo/java/problem1008/Salario.java
@@ -0,0 +1,28 @@
+package Bee.com.italo.java.problem1008;
+
+
+/***
+ * @author italo-mauricio
+ *
+ *                                      Salário
+ *
+ * Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+ * o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+ *
+ * ENTRADA:
+ *       - O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais,
+ *       representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+ *
+ * SAIDA:
+ *       - Imprima o número e o salário do funcionário, conforme exemplo fornecido,
+ *       com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+ *
+ */
+
+
+
+public class Salario {
+    public static void main(String[] args) {
+
+    }
+}
